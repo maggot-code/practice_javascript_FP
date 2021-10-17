ce11cade6da41fbdb98582f050dbd778e01791e9
maggot_code <wowhuanhuan233@163.com>
2021-10-17 13:28:12 +0800

    Update demo01.ts

--- src/demo01.ts
+++ src/demo01.ts
@@ -2,7 +2,7 @@
  * @Author: maggot-code
  * @Date: 2021-04-06 21:08:37
  * @LastEditors: maggot-code
- * @LastEditTime: 2021-04-06 23:01:37
+ * @LastEditTime: 2021-04-08 21:53:24
  * @Description: file content
  */
 interface Person {
